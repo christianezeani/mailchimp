2e17335c7063e77e9990961058e874c652323cb9
Christian Ezeani <christian.ezeani@gmail.com>
2019-05-25 14:47:36 +0100

    Minor updates

--- tests.bootstrap.php
+++ tests.bootstrap.php
@@ -4,3 +4,7 @@ require_once(__DIR__ . '/vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
+$dotenv->required([
+  'MAILCHIMP_LIST_ID',
+  'MAILCHIMP_API_KEY'
+]);
