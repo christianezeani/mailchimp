e332dc7d038eb0aa3abf4d449c9e17e13ae00e9a
Christian Ezeani <christian.ezeani@gmail.com>
2019-05-26 13:08:14 +0100

    phpunit configurations updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
 $dotenv->load();
 
 $dotenv->required([
