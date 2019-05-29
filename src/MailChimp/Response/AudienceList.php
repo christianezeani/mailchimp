568b4ecff5e823096fe7cc76b75f200e2e36c9f9
Christian Ezeani <christian.ezeani@gmail.com>
2019-05-29 16:32:31 +0100

    AudienceList response added

---
+++ src/MailChimp/Response/AudienceList.php
@@ -0,0 +1,24 @@
+<?php
+namespace MailChimp\Response;
+
+use MailChimp\Core\Data;
+use MailChimp\Data\Link;
+use MailChimp\Models\Audience;
+
+
+/**
+ * @property \MailChimp\Models\Audience[] $lists
+ * @property \MailChimp\Data\Link $_links
+ * @property int $total_items
+ */
+class AudienceList extends Data {
+
+  protected $fields = [
+    'lists' => ['type' => Audience::class.'[]'],
+    '_links' => ['type' => Link::class.'[]'],
+    'total_items' => ['type' => 'int'],
+  ];
+
+}
+
+
