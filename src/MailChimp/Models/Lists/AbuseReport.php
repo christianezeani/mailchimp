f8447573826a4fa2aa30f019f70016f24ee376c2
Christian Ezeani <christian.ezeani@gmail.com>
2019-05-30 01:56:55 +0100

    Minor updates

--- src/MailChimp/Models/Lists/AbuseReport.php
+++ src/MailChimp/Models/Lists/AbuseReport.php
@@ -2,6 +2,7 @@
 namespace MailChimp\Models\Lists;
 
 use MailChimp\Core\Model;
+use MailChimp\Core\HashMap;
 
 use MailChimp\Data\Link;
 use MailChimp\Response\AbuseReportListResponse;
@@ -12,13 +13,15 @@ use MailChimp\Response\AbuseReportListResponse;
  * 
  * Manage abuse complaints for a specific list. An abuse complaint occurs when your recipient reports an email as spam in their mail program.
  * 
- * @property float $id
- * @property string $campaign_id
- * @property string $list_id
- * @property string $email_id
- * @property string $email_address
- * @property string $date
- * @property Link[] $links
+ * @property float $id The id for the abuse report
+ * @property string $campaign_id The campaign id for the abuse report
+ * @property string $list_id The list id for the abuse report.
+ * @property string $email_id The MD5 hash of the lowercase version of the list member’s email address.
+ * @property string $email_address Email address for a subscriber.
+ * @property HashMap $merge_fields An individual merge var and value for a member.
+ * @property boolean $vip VIP status for subscriber.
+ * @property string $date Date for the abuse report
+ * @property Link[] $_links A list of link types and descriptions for the API schema documents.
  */
 class AbuseReport extends Model {
 
@@ -36,8 +39,10 @@ class AbuseReport extends Model {
     'list_id' => ['type' => 'string'],
     'email_id' => ['type' => 'string'],
     'email_address' => ['type' => 'string'],
+    'merge_fields' => ['type' => HashMap::class],
+    'vip' => ['type' => 'boolean'],
     'date' => ['type' => 'string'],
-    'links' => ['type' => Link::class.'[]']
+    '_links' => ['type' => Link::class.'[]']
   ];
 
   /**
