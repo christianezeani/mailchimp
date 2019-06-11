a25f33869df9a95438f9dc69ff122d928aceb0e4
Christian Ezeani <christian.ezeani@gmail.com>
2019-06-11 22:20:29 +0100

    Minor fixes

--- src/MailChimp/Framework/Laravel/MailChimpServiceProvider.php
+++ src/MailChimp/Framework/Laravel/MailChimpServiceProvider.php
@@ -9,7 +9,7 @@ use Illuminate\Support\ServiceProvider;
 class MailChimpServiceProvider extends ServiceProvider {
   
   public function register() {
-    $this->app->singleton(MailChimp::class, function ($app) {
+    $this->app->singleton(MailChimp::class, function () {
       $config = new Config(env('MAILCHIMP_API_KEY'));
       return new MailChimp($config);
     });
