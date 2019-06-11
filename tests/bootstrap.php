db2a5a2757063e627b5cbce8a9d10ba356fba2aa
Christian Ezeani <christian.ezeani@gmail.com>
2019-06-12 00:01:50 +0100

    Unit test failure fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,15 +5,15 @@ $dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
 $dotenv->load();
 
 $dotenv->required([
-  'MAILCHIMP_API_KEY',
-  'MAILCHIMP_TEST_EMAIL'
+  'MAILCHIMP_API_KEY'
 ]);
 
 define('MAILCHIMP_API_KEY', getenv('MAILCHIMP_API_KEY'));
-define('MAILCHIMP_TEST_EMAIL', getenv('MAILCHIMP_TEST_EMAIL'));
+// define('MAILCHIMP_TEST_EMAIL', getenv('MAILCHIMP_TEST_EMAIL'));
 
 $emailPrefix = 'mailchimp.' . time();
 
+define('MAILCHIMP_TEST_EMAIL', $emailPrefix.'_1@gmail.com');
 define('MAILCHIMP_TEST_EMAIL_2', $emailPrefix.'_2@gmail.com');
 define('MAILCHIMP_TEST_EMAIL_3', $emailPrefix.'_3@gmail.com');
 
